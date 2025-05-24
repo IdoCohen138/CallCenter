26ea60613009ea5cd7c937514917530b475c6c3f
idoco <idocohen138@gmail.com>
2025-05-24 20:58:20 +0300

    updaet port production

--- server/run.py
+++ server/run.py
@@ -6,4 +6,6 @@ app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=True)
+    debug_mode = os.environ.get("FLASK_DEBUG", "0") == "1"
+    socketio.run(app, host='0.0.0.0', port=port, debug=debug_mode)
+
