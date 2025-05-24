ec9f8892d3c95bd566ae03fd15a69611b61baf6c
idoco <idocohen138@gmail.com>
2025-05-24 22:25:27 +0300

    updaet port production

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
-from werkzeug.middleware.proxy_fix import ProxyFix 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 socketio = SocketIO(cors_allowed_origins="*")
 
 def create_app():
     app = Flask(__name__)
 
+
     app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
-    CORS(app, origins=["*"])
+
+    CORS(app, origins=["https://client-production-ccf0.up.railway.app"])
 
     from .routes import register_routes
     register_routes(app)
@@ -18,5 +21,6 @@ def create_app():
     from .sockets.events import register_socket_events
     register_socket_events(socketio)
 
-    socketio.init_app(app)
+    socketio.init_app(app, cors_allowed_origins="*")  
+
     return app
