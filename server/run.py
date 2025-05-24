3ba0e272d36c6c7a29992f45fe7966d0c8026518
idoco <idocohen138@gmail.com>
2025-05-24 19:30:19 +0300

    updaet port production

--- server/run.py
+++ server/run.py
@@ -10,4 +10,4 @@ print("📍 App created")
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     print(f"📍 Running on port {port}")
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
