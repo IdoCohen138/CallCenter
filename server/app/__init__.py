c3039ec9db1172548e2162e11fe64aadf22cf4e7
idoco <idocohen138@gmail.com>
2025-05-24 21:01:07 +0300

    updaet port production

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
 
@@ -6,16 +6,7 @@ socketio = SocketIO(cors_allowed_origins="*")
 
 def create_app():
     app = Flask(__name__)
-
     CORS(app, origins=["https://client-production-ccf0.up.railway.app"])
-
-    @app.before_request
-    def enforce_https_in_production():
-        if app.env == "production":
-            if request.headers.get("X-Forwarded-Proto", "http") != "https":
-                url = request.url.replace("http://", "https://", 1)
-                return redirect(url, code=301)
-
     from .routes import register_routes
     register_routes(app)
 
--- server/run.py
+++ server/run.py
@@ -6,6 +6,4 @@ app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    debug_mode = os.environ.get("FLASK_DEBUG", "0") == "1"
-    socketio.run(app, host='0.0.0.0', port=port, debug=debug_mode)
-
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
