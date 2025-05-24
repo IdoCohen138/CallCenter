b24f05014d79b7a102d44bdbf4e6298841b071eb
idoco <idocohen138@gmail.com>
2025-05-24 23:32:48 +0300

    updaet port production

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,33 +3,46 @@ from flask_cors import CORS
 from flask_socketio import SocketIO
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-
-socketio = SocketIO(cors_allowed_origins="https://client-production-a326.up.railway.app")
+socketio = SocketIO(cors_allowed_origins=[
+    "https://client-production-a326.up.railway.app",
+    "https://client-production-ccf0.up.railway.app"
+])
 
 def create_app():
     print("📍 Flask app loaded — CORS configured")
 
     app = Flask(__name__)
-
-
     app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
     CORS(
         app,
-        origins=["https://client-production-a326.up.railway.app"],
+        origins=[
+            "https://client-production-a326.up.railway.app",
+            "https://client-production-ccf0.up.railway.app"
+        ],
         supports_credentials=True,
-        resources={r"/api/*": {"origins": "https://client-production-a326.up.railway.app"}}
+        resources={
+            r"/api/*": {
+                "origins": [
+                    "https://client-production-a326.up.railway.app",
+                    "https://client-production-ccf0.up.railway.app"
+                ]
+            }
+        }
     )
-
 
     from .routes import register_routes
     register_routes(app)
 
     from .sockets.events import register_socket_events
     register_socket_events(socketio)
+
     socketio.init_app(
         app,
-        cors_allowed_origins="https://client-production-a326.up.railway.app",
+        cors_allowed_origins=[
+            "https://client-production-a326.up.railway.app",
+            "https://client-production-ccf0.up.railway.app"
+        ],
         logger=True,
         engineio_logger=True
     )
