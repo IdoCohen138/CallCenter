626bb1b17b04457bb84b024f9b209ffe834b307c
idoco <idocohen138@gmail.com>
2025-05-24 22:43:12 +0300

    updaet port production

--- server/app/routes/users.py
+++ server/app/routes/users.py
@@ -1,10 +1,21 @@
-from flask import Blueprint, jsonify
-from app.services.database import get_all_users
+# from flask import Blueprint, jsonify
+# from app.services.database import get_all_users
+
+# users_bp = Blueprint('users', __name__)
+
+# @users_bp.route('/', methods=['GET'])
+# def fetch_users():
+#     print("📍 get_all_users called")
+#     return jsonify(get_all_users())
+
+from flask import request, jsonify, Blueprint
 
 users_bp = Blueprint('users', __name__)
 
-@users_bp.route('/', methods=['GET'])
-def fetch_users():
-    print("📍 get_all_users called")
-    return jsonify(get_all_users())
-
+@users_bp.route("/check", methods=["GET"])
+def check_request_info():
+    return jsonify({
+        "scheme": request.scheme,
+        "url": request.url,
+        "headers": dict(request.headers)
+    })
