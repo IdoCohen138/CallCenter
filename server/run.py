7a21f015b9d450d08cf46b927fe33eb03b80f3f4
idoco <idocohen138@gmail.com>
2025-05-24 18:16:56 +0300

    updaet port production

--- server/run.py
+++ server/run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
