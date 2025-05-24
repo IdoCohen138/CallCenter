7c20bf633e029906685667b427070eef3ca587ed
idoco <idocohen138@gmail.com>
2025-05-24 22:02:32 +0300

    updaet port production

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
 
-socketio = SocketIO(cors_allowed_origins="https://client-production-ccf0.up.railway.app")
+socketio = SocketIO(cors_allowed_origins="*")
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins=["https://client-production-ccf0.up.railway.app"])
+    CORS(app, origins=["*"])
     from .routes import register_routes
     register_routes(app)
 
